213932ecfd56169df689473bb52ec7fa8eafeff2
stasya-sugovdz <sugovdz.s90@gmail.com>
2017-07-26 18:08:18 +0300

    Created string transform functionality

--- app/js/libs/lesson5.js
+++ app/js/libs/lesson5.js
@@ -1,14 +1,4 @@
-// var i = 0;
-// function onStart() {
-//   var timerId = setTimeout(function () {
-//       if(i < 30) {
-//         i++;
-//         (i < 10) ? document.getElementById('seconds').innerHTML = '0' + i : document.getElementById('seconds').innerHTML = '' + i;
-//         onStart();
-//       }
-//     }, 1000);
-//   return timerId;
-// }
+//timer functionality
 var i = 1;
 var go = true;
 var reverse = false;
@@ -54,3 +44,45 @@ function onReverse() {
   timer();
 }
 
+//string transformation
+
+function getText() {
+  var text = prompt('Please, enter text', 'some text');
+  var newText = text.replace(',', '').replace('.', '').replace('!', '').replace('?', '');
+  if(newText) {
+    outputText(newText);
+    countA(newText)
+  }
+  // var parent = document.querySelector('.str-transform__result');
+  // var children = parent.querySelectorAll('li');
+  // if( children.length !== 0) {
+  //   for(var i = 0; i < children.length; i++) {
+  //     parent.removeChild(children[i]);
+  //   }
+  // }
+}
+
+function outputText(val) {
+  var arr = val.split(' ');
+
+  var parent = document.querySelector('.str-transform__result');
+  var newItem = document.createElement('LI');
+  var place = parent.appendChild(newItem);
+
+  place.innerHTML = arr[0].toUpperCase();
+
+  for(var i = 1; i < arr.length; i++) {
+    var currItem = document.createElement('LI');
+    var currPlace = parent.appendChild(currItem);
+    currPlace.innerHTML = arr[i];
+
+    if(arr[(arr.length - 1)] || arr[(arr.length - 2)]) {
+      currPlace.innerHTML = arr[i].toLowerCase();
+    }
+  }
+}
+
+function countA(val) {
+  var counter = val.split(/a/gi).length - 1;
+  (counter) ? alert('Text includes: ' + counter + ' "a"') : alert('There is no "a" in the text');
+}
